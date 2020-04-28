ffc79c82eb8c4c246bc046da762be3a8e2de3ba5
chenweidong <382564@qq.com>
2020-04-28 17:22:36 +0800

    init

---
+++ src/main/java/com/example/demo/web/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo.web;
+
+import com.example.demo.pojo.Result;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ab")
+public class DemoController {
+
+    @RequestMapping("/cd")
+    public Result testHttpClient() {
+        Result result = new Result();
+        result.setCode(200);
+        result.setMsg("操作成功");
+        return result;
+    }
+}
